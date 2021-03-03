0155a5e6ae9a97704b38c06938e26ae625f2e692
Sri <srilakshmi@gmail.com>
2021-03-03 17:23:07 +0530

    made changes to first.java program

--- first.java
+++ first.java
@@ -1,2 +1,17 @@
-// This is empty file
-//
+importjava.util*;
+public class NS{
+	public static void main (String[] args){
+		Scanner sc=newScanner(System.in);
+		int n=sc.nextInt();
+		int p=n+1;
+		int arr[]=newint[p];
+		for(int i=0;i<p;i++){
+			arr[i]=i;
+		}
+		int sum=0;
+		for(int i=0;i<p;i++){
+			sum=sum+arr[i];
+		}
+		System.out.println(sum);
+	        }
+	        }/
